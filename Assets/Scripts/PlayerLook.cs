733460f7901114341ae3087ea34506a5f8624d2f
Spymaster667 <132275001+Spymaster667@users.noreply.github.com>
2025-11-28 13:06:35 +0000

    Begin implementation of player

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+	// ----- Private
+	private GameObject player;
+
+	private void Start()
+	{
+	    
+	}
+
+	void Update()
+	{
+		
+	}
+}
