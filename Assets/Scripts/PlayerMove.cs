2d453736b7473b854de0f6362a908fba3a9cc3d0
Spymaster667 <132275001+Spymaster667@users.noreply.github.com>
2025-12-04 23:24:53 +0000

    Begin implementing carriage sfx system
    Consider changing over to a simple enum state machine, or maybe even just a statemachine.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine.InputSystem;
 using UnityEngine.Splines;
 
@@ -12,30 +11,35 @@ public class PlayerMove : MonoBehaviour
 	public float accelerationTime = 1f;
 	public float maxSpeed = .01f;
 	
+	// Audio
+	[Header("Audio")]
+
+	public AudioClip carriageAccel;
+	public AudioClip carriageDrone;
+	public AudioClip carriageDecel;
+	public AudioClip carriageStop;
+
+	public float droneToDecelFade = 0.1f;
+	
 	// ----- Private
 	// References
 	private Rigidbody rb;
-	private AudioSource as0;
-	private AudioSource as1;
+	private AudioSource[] sources;
 	
 	// Varyings
-	private float2 moveInput;
-	private float velocity;
+	private float2 moveInput = float2.zero;
+	private float velocity = 0;
 
 	private float3 railPosition = float3.zero;
 
-	private bool asIdentifier;
+	private bool playAccelerating = true;
 
 	private void Start()
 	{
 		AttachToRail(rail);
 		rb = GetComponent<Rigidbody>();
 		
-		as0 = GetComponent<AudioSource>();
-		as1 = GetComponent<AudioSource>();
-		
-		print(as0);
-		print(as1);
+		sources = GetComponents<AudioSource>();
 	}
 	
 	private void Update()
@@ -45,6 +49,7 @@ public class PlayerMove : MonoBehaviour
 		if (math.abs(moveInput.x) > Mathf.Epsilon || math.abs(moveInput.y) > Mathf.Epsilon)
 		{
 			velocity = Mathf.MoveTowards(velocity, maxSpeed * moveInput.y, accelDelta);
+			PlayCarriageStart();
 		}
 		else
 		{
@@ -72,5 +77,49 @@ public class PlayerMove : MonoBehaviour
 	{
 		rail = container;
 		railPosition = container.transform.position;
+	}
+
+	private void PlayCarriageStart()
+	{
+		// reset volume and clip on low volume
+		if (sources[0].volume < 1 - float.Epsilon)
+		{
+			sources[0].Stop();
+			sources[0].volume = 1;
+			playAccelerating = true;
+		}
+		
+		// don't change if playing
+		if (sources[0].isPlaying)
+		{
+			return;
+		}
+
+		// play accelerating or drone
+		if (playAccelerating)
+		{
+			sources[0].PlayOneShot(carriageAccel);
+			playAccelerating = false;
+		}
+		else
+		{
+			sources[0].loop = true;
+			sources[0].clip = carriageDrone;
+			sources[0].Play();
+		}
+
+		return;
+	}
+
+	private void PlayCarriageStop()
+	{
+		sources[0].volume = Mathf.MoveTowards(sources[0].volume, 0, droneToDecelFade * Time.deltaTime);
+
+		if (sources[1].isPlaying)
+		{
+			return;
+		}
+		
+		sources[1].PlayOneShot(carriageDecel);
 	}
 }
